a8f8f2c9057144b447d6b35f8d40fd9cf6fcb45f
Denis <thesocot@mail.ru>
2023-02-22 17:26:11 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import {Navbar,Hero, Stats, Business, Billing, CardDeal, Testimonials, Clients, CTA, Footer} from './components/index'
+
+
+import styles from './style'
+const App = () => (
+    <div className='bg-primary w-full overflow-hidden'>
+      <div className={`${styles.paddingX} ${styles.flexCenter}` }>
+        <div className={`${styles.boxWidth}`}>
+            <Navbar/>
+        </div>
+      </div>
+
+      <div className={`bg-primary ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Hero/>
+        </div>
+      </div>
+      
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Stats/>
+          <Business/>
+          <Billing/>
+          <CardDeal/>
+          <Testimonials/>
+          <Clients/>
+          <CTA/>
+          <Footer/>
+        </div>
+      </div>
+    </div>
+  )
+
+export default App
